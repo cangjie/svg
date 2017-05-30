a24bb881e944d43e5a0bf9aba69427c95e69cc7f
cangjie <cangjie@hotmail.com>
2017-05-30 10:40:53 +0800

    zuobiao

---
+++ js/stock.js
@@ -0,0 +1,29 @@
+/**
+ * Created by sunli on 17/5/30.
+ */
+function get_max_price(stock_data, start_index, end_index) {
+    var max_price = 0;
+    for(var i = start_index; i < stock_data.length && i <= end_index; i++ ) {
+        if (i==start_index) {
+            max_price = parseFloat(stock_data[i].item_hignest_price);
+        }
+        else {
+            max_price = Math.max(max_price, parseFloat(stock_data[i].item_hignest_price));
+        }
+    }
+    return max_price;
+}
+
+function get_min_price(stock_data, start_index, end_index) {
+    var min_price = 0;
+    for(var i = start_index; i < stock_data.length && i <= end_index ; i++ ) {
+        if (i==start_index) {
+            min_price = parseFloat(stock_data[i].item_hignest_price);
+        }
+        else {
+            min_price = Math.min(min_price, parseFloat(stock_data[i].item_lowest_price));
+        }
+    }
+    return min_price;
+}
+
